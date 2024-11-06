e34bfcb172c74b1df66ee9af712ea72af8327122
sara <srguzman260@gmail.com>
2024-11-06 13:01:23 -0500

    SOBRE NOSOTROS

---
+++ script.js
@@ -0,0 +1,26 @@
+function handleMobileMenu() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Iniciar animaciones y funcionalidades cuando el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+    startTypingAnimation();
+    animateOnScroll();
+    handleMobileMenu();
+});
